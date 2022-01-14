bbd16b3593a7d70475f2644c0e55adc9996ac3ac
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-01-13 22:41:49 -0300

    Keeper App - Part 2
    - Add Sass
    - Solve TypeScript problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Header } from './components/Header'
-import {Note} from './components/Note'
+import { Note } from './components/Note'
 import { Footer } from './components/Footer'
+import { notes } from './notes'
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Note />
+        {notes.map((note, index)=>(
+          <Note key={index} title={note.title} content={note.content}/>
+        ))}
       </main>
       <Footer />
     </>
